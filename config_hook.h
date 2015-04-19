3b7bb1f3591c1de3a700eec8e6d07594d5b535f1
rlbossman <rlbossman@gmail.com>
2015-04-19 00:08:46 -0500

    use enum

--- config_hook.h
+++ config_hook.h
@@ -8,18 +8,11 @@ int _APPEND_work(char **__new, int *_buf_len, char *filename, char *append);
 int _EXPLICIT_work(char **__new, char *config_name, char *redefine, char *old, char **buf, int *_buf_len);
 
 
-/* aligned in vim, sorry... */
-#define MASK_64 						0x0000000000000000
-
-#define PermitRootLogin_EXPLICIT		0x0000000000000001
-#define PermitRootLogin_APPEND			0x0000000000000010
-#define PermitRootLogin_NOWORK			0x0000000000000011
-#define PermitRootLogin_MASK			0x0000000000000011
-
-#define PasswordAuthentication_EXPLICIT 0x0000000000000100
-#define PasswordAuthentication_APPEND 	0x0000000000001000
-#define PasswordAuthentication_NOWORK 	0x0000000000001100
-#define PasswordAuthentication_MASK	 	0x0000000000001100
+enum worktype {
+	APPEND,
+	EXPLICIT,
+	NOWORK
+};
 
 
 #define RELOC_ADDEND 1
